cad913d370babde9af3e26db9038a4aa0b85dd40
Andrew Pigram <andrew.pigram@fivium.co.uk>
2013-09-10 14:35:27 +1000

    Updated test case to create three plain-text changelog files to test specific line endings. Updated POM to wind back version of jersey-multiparts library (v1.8 was causing testing issues. This may need to be reverted if test cases fail again. This will, however, have no impact on the build process.)

--- test/net/foxopen/jira/changelog/ChangelogTemplateTest.java
+++ test/net/foxopen/jira/changelog/ChangelogTemplateTest.java
@@ -11,75 +11,142 @@ import junit.framework.TestCase;
 import java.util.LinkedList;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.fail;
+import java.util.Properties;
+import junit.framework.Assert;
 
 /**
  * Test case for generating file and module changelog templates
+ *
  * @author apigram
  */
 public class ChangelogTemplateTest extends TestCase {
-		LinkedList<Change> issues;
-		List<VersionInfo> versions;
-		StringWriter output;
-	
-	public ChangelogTemplateTest(String testName) {
-		super(testName);
-	}
-	
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		issues = new LinkedList<Change>();
-		
-		issues.add(new Change("TESTPROJ-10", "Test Issue", "Bug"));
-		issues.add(new Change("TESTPROJ-20", "Test Issue 2", "Task"));
-		issues.add(new Change("TESTPROJ-30", "Test Issue 3", "Support Ticket"));
-		issues.add(new Change("TESTPROJ-40", "Test Issue 4", "Task"));
-		
-		versions = new LinkedList<VersionInfo>();
-		VersionInfo version = new VersionInfo("2.0.1", "Bug fix release for 2.0.0", new Date(), issues);
-		versions.add(version);
-		version = new VersionInfo("2.0.2", "Bug fix release for 2.0.0", new Date(), issues);
-		versions.add(version);
-		
-		
-		output = new StringWriter();
-	}
-	
-	@Override
-	protected void tearDown() throws Exception {
-		output.close();
-		super.tearDown();
-	}
 
-	/**
-	 * Black-box unit test of createChangelog method, of class ChangelogTemplate.
-	 */
-	public void testFileChangelog() throws Exception {
-		System.out.println("fileChangelog");
-		try {
-			ChangelogTemplate.createChangelog(versions, output, "examples/file.mustache", LineEnding.NATIVE);
-		} catch (Exception e) {
-			fail("Exception raised.");
-			e.printStackTrace();
-		}
-		
-		assertNotNull("No file output.", output.toString());
-		System.out.println(output.toString());
-	}
-	
-	/**
-	 * Black-box unit test of createChangelog method, of class ChangelogTemplate.
-	 */
-	public void testModuleChangelog() throws Exception {
-		System.out.println("moduleChangelog");
-		try {
-			ChangelogTemplate.createChangelog(versions, output, "examples/module.mustache", LineEnding.NATIVE);
-		} catch (Exception e) {
-			fail("Exception raised.");
-			e.printStackTrace();
-		}
-		
-		assertNotNull("No file output.", output.toString());
-		System.out.println(output.toString());
-	}
+  LinkedList<Change> issues;
+  List<VersionInfo> versions;
+  StringWriter output;
+
+  public ChangelogTemplateTest(String testName) {
+    super(testName);
+  }
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    issues = new LinkedList<Change>();
+
+    issues.add(new Change("TESTPROJ-10", "Test Issue", "Bug"));
+    issues.add(new Change("TESTPROJ-20", "Test Issue 2", "Task"));
+    issues.add(new Change("TESTPROJ-30", "Test Issue 3", "Support Ticket"));
+    issues.add(new Change("TESTPROJ-40", "Test Issue 4", "Task"));
+
+    versions = new LinkedList<VersionInfo>();
+    VersionInfo version = new VersionInfo("2.0.1", "Bug fix release for 2.0.0", new Date(), issues);
+    versions.add(version);
+    version = new VersionInfo("2.0.2", "Bug fix release for 2.0.0", new Date(), issues);
+    versions.add(version);
+
+    output = new StringWriter();
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    output.close();
+    super.tearDown();
+  }
+
+  /**
+   * White-box unit test of createChangelog method, of class ChangelogTemplate.
+   */
+  public void testTextChangelog() throws Exception {
+    System.out.println("textChangelog");
+    try {
+      ChangelogTemplate.createChangelog(versions, output, "examples/plain-text.mustache", LineEnding.NATIVE);
+    } catch (Exception e) {
+      fail("Exception raised.");
+      e.printStackTrace();
+    }
+
+    assertNotNull("No file output.", output.toString());
+    System.out.println(output.toString());
+  }
+
+  /**
+   * White-box unit test of createChangelog method, of class ChangelogTemplate.
+   */
+  public void testHTMLChangelog() throws Exception {
+    System.out.println("HTMLChangelog");
+    try {
+      ChangelogTemplate.createChangelog(versions, output, "examples/html.mustache", LineEnding.NATIVE);
+    } catch (Exception e) {
+      fail("Exception raised.");
+      e.printStackTrace();
+    }
+
+    assertNotNull("No file output.", output.toString());
+    System.out.println(output.toString());
+  }
+
+  /**
+   * White-box unit test of createChangelog method, of class ChangelogTemplate.
+   */
+  public void testXMLChangelog() throws Exception {
+    System.out.println("XMLChangelog");
+    try {
+      ChangelogTemplate.createChangelog(versions, output, "examples/xml.mustache", LineEnding.NATIVE);
+    } catch (Exception e) {
+      fail("Exception raised.");
+      e.printStackTrace();
+    }
+
+    assertNotNull("No file output.", output.toString());
+    System.out.println(output.toString());
+  }
+
+  /**
+   * Black-box integration test for a complete run of the program.
+   *
+   * @throws Exception
+   */
+  public void testFullRun() throws Exception {
+    Properties properties = new Properties();
+    properties.load(new FileInputStream("credentials.properties"));
+    System.out.println("fullRun");
+    String[] args = new String[9];
+    args[0] = properties.getProperty("url");
+    args[1] = properties.getProperty("username");
+    args[2] = properties.getProperty("password");
+    args[3] = properties.getProperty("project");
+    args[4] = properties.getProperty("version");
+    args[5] = "examples/html.mustache";
+    args[6] = "--debug";
+    args[7] = "--changelog-file-name";
+    args[8] = "changelog.html";
+
+    // wrapper function has same effect as main, minus the System.exit call.
+    Changelog.main(args);
+
+    File f = new File("changelog.html");
+    assertTrue(f.exists());
+  }
+  
+  public void testLineEndings() throws Exception  {
+    System.out.println("lineEndings");
+    FileWriter out1 = new FileWriter("testNative.txt");
+    FileWriter out2 = new FileWriter("testWindows.txt");
+    FileWriter out3 = new FileWriter("testNIX.txt");
+    try {
+      ChangelogTemplate.createChangelog(versions, out1, "examples/plain-text.mustache", LineEnding.NATIVE);
+      ChangelogTemplate.createChangelog(versions, out2, "examples/plain-text.mustache", LineEnding.WINDOWS);
+      ChangelogTemplate.createChangelog(versions, out3, "examples/plain-text.mustache", LineEnding.NIX);
+    } catch (Exception e) {
+      fail("Exception raised.");
+      e.printStackTrace();
+    }
+    
+    // manually check the result for this test case
+    out1.close();
+    out2.close();
+    out3.close();
+    assertTrue(true);
+  }
 }
